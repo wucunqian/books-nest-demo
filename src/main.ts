db5d2c4568891761f4805f0439b221d5b55a28e6
wucunqian <2801531707@qq.com>
2024-07-28 00:01:31 +0800

    fix：开启跨域

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  // 开启后端跨域
+  app.enableCors();
 
   await app.listen(3000);
 }
